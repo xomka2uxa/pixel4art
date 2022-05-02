598e7fcf6e23bf6612303f9b710f492a6d323c81
«Антон» <nirvs@mail.ru>
2022-05-02 22:42:44 +0300

    'iss1'

---
+++ client/src/views/PostsView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="posts">
+    <h1>Посты</h1>
+    <table>
+      <tr>
+        <th>
+          Заголовок
+        </th>
+        <th>
+          Описание
+        </th>
+      </tr>
+      <tr v-for='(post, i) in posts' :key='i'>
+        <td>{{ post.title }}</td>
+        <td>{{ post.description }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService';
+
+export default {
+  name: 'PostsView',
+
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getPosts() {
+      const response = await PostsService.fetchPosts();
+      console.log(response, 333);
+      this.posts = response.data;
+    },
+  },
+
+  mounted() {
+    this.getPosts();
+  },
+};
+</script>
