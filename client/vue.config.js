12aa33d0ec9b111a2f3b86236a0f818e8fce831f
Галина <xomka2uxa@mail.ru>
2022-08-30 01:49:13 +0300

    work in progress

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
+    sourceMap: true,
     loaderOptions: {
       scss: {
         additionalData: `@import "@/assets/scss/shared/variables.scss";`,
